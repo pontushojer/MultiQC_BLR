9f11940e6bfe0dc871acf627ee86592df7e59a0a
Pontus Hojer <pontushojer@gmail.com>
2020-05-08 17:47:58 +0200

    Add test for phaseblock parsing

--- tests/tests.py
+++ tests/tests.py
@@ -3,10 +3,13 @@ from shutil import copyfile
 import subprocess
 
 TESTDATA_STATS = Path("tests/data/example_stats.log")
-TESTDATA_HAPCUT2 = Path("tests/data/example_hapcut2_phasing_stats.txt")
+TESTDATA_HAPCUT2_PHASING_STATS = Path("tests/data/example_hapcut2_phasing_stats.txt")
+TESTDATA_HAPCUT2_PHASEBLOCK = Path("tests/data/example_hapcut2.phase")
+
 
 EXPECTED_OUTPUT_STATS = Path("tests/expected_output/example_stats.txt")
-EXPECTED_OUTPUT_HAPCUT2 = Path("tests/expected_output/hapcut2_phasing_stats.txt")
+EXPECTED_OUTPUT_HAPCUT2_PHASING_STATS = Path("tests/expected_output/hapcut2_phasing_stats.txt")
+EXPECTED_OUTPUT_HAPCUT2_PHASEBLOCK_LEN = Path("tests/expected_output/hapcut2_phaseblock_lengths.txt")
 
 
 def comp_files_linewise(file1: Path, file2: Path):
@@ -28,12 +31,17 @@ def test_stats(tmpdir):
 
 
 def test_hapcut2(tmpdir):
-    copyfile(TESTDATA_HAPCUT2, tmpdir / "example.txt")
+    copyfile(TESTDATA_HAPCUT2_PHASING_STATS, tmpdir / "example.txt")
+    copyfile(TESTDATA_HAPCUT2_PHASEBLOCK, tmpdir / "example.phase")
 
     subprocess.run(["multiqc", "-f", tmpdir, "-o", tmpdir])
 
     assert Path(tmpdir / "multiqc_report.html").exists()
     assert Path(tmpdir / "multiqc_data" / "hapcut2_phasing_stats.txt").exists()
+    assert Path(tmpdir / "multiqc_data" / "hapcut2_phaseblock_lengths.txt").exists()
 
     comp_files_linewise(Path(tmpdir / "multiqc_data" / "hapcut2_phasing_stats.txt"),
-                        EXPECTED_OUTPUT_HAPCUT2)
+                        EXPECTED_OUTPUT_HAPCUT2_PHASING_STATS)
+    comp_files_linewise(Path(tmpdir / "multiqc_data" / "hapcut2_phaseblock_lengths.txt"),
+                        EXPECTED_OUTPUT_HAPCUT2_PHASEBLOCK_LEN)
+
